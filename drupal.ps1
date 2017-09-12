0a4dae6ed34f00a1d50d656dca6fd5c6cff82306
Marco Boom <m.boom@student.tudelft.nl>
2017-09-12 11:10:34 +0200

    Docker- and setupfiles added

---
+++ drupal.ps1
@@ -0,0 +1,37 @@
+# Install Drupal in a Windows Server environment
+
+# Define user account information
+$postgresusr = "postgres"
+$drupalusr = "drupal"
+$drupalpw = "drupal"
+$drupaldb = "drupal"
+
+# Define download URL
+$drupalurl = "https://ftp.drupal.org/files/projects/drupal-8.3.7.zip"
+$drupal = "C:\site"
+
+# Download Drupal package
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $drupalurl -OutFile drupal.zip
+Expand-Archive -Path drupal.zip -DestinationPath $drupal
+Remove-Item drupal.zip
+move C:\site\drupal-8.3.7\* $drupal
+Remove-Item -R $drupal\drupal-8.3.7
+cd $drupal
+Start-Process composer -NoNewWindow -Wait -ArgumentList "require drush/drush"
+cd C:\
+setx PATH /M ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path + ";$drupal\vendor\bin")
+$env:Path += ";$drupal\vendor\bin"
+
+# Create Drupal user and database
+Start-Process psql -NoNewWindow -Wait -ArgumentList "--command `"CREATE USER $drupalusr PASSWORD '$drupalpw';`"", "--command `"CREATE DATABASE $drupaldb OWNER $drupalusr;`"", "--quiet", "--username $postgresusr"
+
+# Create New IIS Site
+Import-Module IISAdministration
+New-IISSite -BindingInformation *:80: -Name Drupal -PhysicalPath $drupal
+
+# Remove Default Web Site and Start Drupal website
+Stop-IISSite -Confirm:$false -Name "Default Web Site"
+Remove-IISSite -Confirm:$false -Name "Default Web Site"
+
+Remove-Item drupal.ps1
